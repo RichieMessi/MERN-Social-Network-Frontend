caffe6927eb3842cbc01c93130047dbbb3481893
Richardson <coderichardson@gmail.com>
2019-06-28 16:59:55 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import NavbarComponent from './components/Navbar' 
+
+// import main from './main.css'
+import { BrowserRouter as Router } from 'react-router-dom'
+
+import MainRouterComponent from './routes/MainRouter'
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="">
+        <Router>
+        <NavbarComponent />
+          <MainRouterComponent />
+        </Router>
       </div>
     );
   }
